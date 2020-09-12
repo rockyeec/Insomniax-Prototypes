2865b5f119032d5a486edb3e5dc3302ad7331c3e
Rock <0123956@kdu-online.com>
2020-09-12 13:11:44 +0800

    implemented shooting

--- Prototypes GPS2/Assets/Prototypes/TowerDefense/Scripts/TowerScript.cs
+++ Prototypes GPS2/Assets/Prototypes/TowerDefense/Scripts/TowerScript.cs
@@ -5,16 +5,42 @@ using UnityEngine;
 
 public class TowerScript : MonoBehaviour
 {
+    [SerializeField] private GameObject bullet = null;
     [SerializeField] private Transform gun = null;
     [SerializeField] private Transform turret = null;
 
+    FireVariables fireVariables = new FireVariables();
+    class FireVariables
+    {
+        float interval = 0.3f;
+        float time = 0.0f;
+
+        public bool IsNextInterval()
+        {
+            if (Time.time >= time)
+            {
+                time = Time.time + interval;
+                return true;
+            }
+            return false;
+        }
+    }   
 
     private void Update()
     {
-        LookAt();
+        float delta = Time.deltaTime;
+        LookAt(delta);
     }
 
-    void LookAt()
+    void Fire(float delta)
+    {
+        if (fireVariables.IsNextInterval())
+        {
+            Instantiate(bullet, gun.position, gun.rotation);
+        }
+    }
+
+    void LookAt(float delta)
     {
         Collider[] colliders = Physics.OverlapSphere(turret.position, 16.9f, 1 << 10);
         if (colliders.Length != 0)
@@ -26,15 +52,16 @@ public class TowerScript : MonoBehaviour
             Vector3 turretDir = nearest.transform.position - turret.position;
             turretDir.y = 0.0f;
             Quaternion towerRot = Quaternion.LookRotation(turretDir);
-            turret.rotation = Quaternion.Slerp(turret.rotation, towerRot, 6.9f * Time.deltaTime);
+            turret.rotation = Quaternion.Slerp(turret.rotation, towerRot, 6.9f * delta);
 
             Vector3 gunDir = nearest.transform.position - gun.position;
             Quaternion gunRot = Quaternion.LookRotation(gunDir);
             Vector3 gunEul = gunRot.eulerAngles;
             gunEul.y = 0.0f;
             gunRot.eulerAngles = gunEul;
-            gun.localRotation = Quaternion.Slerp(gun.localRotation, gunRot, 6.9f * Time.deltaTime);
+            gun.localRotation = Quaternion.Slerp(gun.localRotation, gunRot, 6.9f * delta);
 
+            Fire(delta);
         }
     }
 }
