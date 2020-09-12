b4871bc17b20cf43855f98c9bf979eb4cc855d93
Rock <0123956@kdu-online.com>
2020-09-12 16:40:43 +0800

    added halloween kids route

--- Prototypes GPS2/Assets/Prototypes/TowerDefense/Scripts/HalloweenKid.cs
+++ Prototypes GPS2/Assets/Prototypes/TowerDefense/Scripts/HalloweenKid.cs
@@ -2,22 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[SerializeField]
+//[System.Serializable]
 public class KidsRoute
 {
-    public List<Transform> points;
+    public Transform[] points;
 }
 
 
 public class HalloweenKid : MonoBehaviour
 {
-    void Start()
+    Queue<Vector3> route = new Queue<Vector3>();
+    Vector3 curPoint;
+
+    public void Init(KidsRoute route)
     {
-        
+        foreach (var item in route.points)
+        {
+            this.route.Enqueue(item.position);
+        }
+        curPoint = this.route.Dequeue();
     }
 
     void Update()
     {
-        
+        if (Vector3.Distance(curPoint, transform.position) <= 2.5f)
+        {
+            if (route.Count != 0)
+                curPoint = route.Dequeue();
+        }
+        else
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, curPoint, 6.9f * Time.deltaTime);
+            transform.position = pos;
+        }
     }
 }
--- Prototypes GPS2/Assets/Prototypes/TowerDefense/Scripts/TowerDefenseGameMaanager.cs
+++ Prototypes GPS2/Assets/Prototypes/TowerDefense/Scripts/TowerDefenseGameMaanager.cs
@@ -5,12 +5,27 @@ using UnityEngine.UI;
 
 public class TowerDefenseGameMaanager : MonoBehaviour
 {
-    [SerializeField] private Button tower1Button;
-    [SerializeField] private GameObject tower;
-    [SerializeField] private Transform towersParent;
+    [SerializeField] private Transform routeParent;
+    [SerializeField] private GameObject kid = null;
+    [SerializeField] private Button tower1Button = null;
+    [SerializeField] private GameObject tower = null;
+    [SerializeField] private Transform towersParent = null;
 
     // temp
     public KidsRoute route = new KidsRoute();
+
+    private void Awake()
+    {        
+        Transform[] points = routeParent.GetComponentsInChildren<Transform>();
+        List<Transform> pointList = new List<Transform>();
+        foreach (var item in points)
+        {
+            pointList.Add(item);
+        }
+        if (pointList.Contains(routeParent))
+            pointList.Remove(routeParent);
+        route.points = pointList.ToArray();
+    }
 
     private void Start()
     {
@@ -20,5 +35,22 @@ public class TowerDefenseGameMaanager : MonoBehaviour
     void CreateTower1()
     {
         Instantiate(tower, towersParent);
+    }
+
+    float interval = 0.69f;
+    float time = 0.0f;
+
+    private void Update()
+    {
+        if (Time.time >= time)
+        {
+            time = Time.time + interval;
+
+            HalloweenKid localKid = Instantiate(kid, route.points[0].position, Quaternion.identity).GetComponent<HalloweenKid>();
+            if (localKid != null)
+            {
+                localKid.Init(route);
+            }
+        }
     }
 }
