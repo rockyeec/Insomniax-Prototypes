f7e8654498d5e8f4bdfd22213d7e9c82869ca1fd
Wan Hazwan <wahazwan1@gmail.com>
2020-09-18 17:09:27 +0800

    Tile Game - Level 2
    Might do a bit of changes later. This is for now

---
+++ View/Assets/Hazwan/Scripts/FallingTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingTile : MonoBehaviour
+{
+    public GameObject cube;
+    public bool isFake = false;
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "GroundCheck" && isFake)
+        {
+            Rigidbody cubeRigidbody = cube.GetComponent<Rigidbody>();
+            cubeRigidbody.isKinematic = false;
+        }
+    }
+}
---
+++ View/Assets/Hazwan/Scripts/TilePattern.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilePattern : MonoBehaviour
+{
+    public GameObject Tile;
+    public GameObject FakeTile;
+    public float gridX = 5f;
+    public float gridY = 5f;
+    public float spacing = 2f;
+
+    void Start()
+    {
+        for (int y = 0; y < gridY; y++)
+        {
+            for (int x = 0; x < gridX; x++)
+            {
+                Vector3 pos = new Vector3(x, 0, y) * spacing;
+                if((y == 0 && x == 2) || (y == 1 && x == 3) || (y == 2 && x == 2) || (y == 3 && x == 1) || (y == 4 && x == 0))
+                {
+                    Instantiate(Tile, pos, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(FakeTile, pos, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
