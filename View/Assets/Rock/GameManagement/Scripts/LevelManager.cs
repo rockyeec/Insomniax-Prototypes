d03c194974aa5d409f5dfbe8eb98d2c014b672b5
Rock <0123956@kdu-online.com>
2020-09-19 15:31:27 +0800

    Added temp reset button for build

---
+++ View/Assets/Rock/GameManagement/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LevelManager : MonoBehaviour
+{
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
