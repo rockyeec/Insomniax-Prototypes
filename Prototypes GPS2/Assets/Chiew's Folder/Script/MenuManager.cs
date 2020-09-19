d1217d09ad77d5dc79965a9d92ee14a80fade548
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-09-19 17:47:43 +0800

    MainMenuUI_Script_Update

---
+++ Prototypes GPS2/Assets/Chiew's Folder/Script/MenuManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public enum PagesList
+{
+    MAIN_MENU = 0,
+    OPTION_PAGE,
+    INSTRUCTION_PAGE
+}
+
+public class MenuManager : MonoBehaviour
+{
+    public MainMenuPages[] Pages; //Use array because optimize with function array provided
+    public GameObject BackButton;
+    public GameObject MenuParents;
+
+    void Start()
+    {
+        BacktoMainMenu();
+    }
+
+    public void BacktoMainMenu()
+    {
+        MenuParents.SetActive(true);
+        foreach (MainMenuPages page in Pages)
+        {
+            if (page.type == 0)
+            {
+                page.self.SetActive(true);
+                BackButton.SetActive(false);
+            }
+            else
+            {
+                page.self.SetActive(false);
+                BackButton.SetActive(false);
+            }
+        }
+    }
+    
+    public void OpenPage(int pageIndex)
+    {
+        MainMenuPages m = Array.Find(Pages, page => page.type == (PagesList)pageIndex); //Find Each Page you wanted
+        if(m == null)
+        {
+            Debug.LogWarning("Page: " + (PagesList)pageIndex + " not found!");
+            return;
+        }
+        if(pageIndex != 0)
+        {
+            foreach (MainMenuPages page in Pages)
+            {
+                if (page.type == 0)
+                {
+                    page.self.SetActive(false);
+                    BackButton.SetActive(true);
+                }
+            }
+        }
+        m.self.SetActive(true);
+    }
+
+    public void CloseMainMenu()
+    {
+        MenuParents.SetActive(false);
+    }
+
+    public void CloseApplication()
+    {
+        Debug.Log("QuitGame");//Debug Purpose;
+        Application.Quit();
+    }
+
+}
+
+[System.Serializable]
+public class MainMenuPages
+{
+    public string pageName;
+    public PagesList type;
+    public GameObject self;
+}
