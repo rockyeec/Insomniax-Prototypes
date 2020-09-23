aa2a9b2d1847f989e46ebf7b75e7ae3d0207abfc
Rock <0123956@kdu-online.com>
2020-09-23 22:13:18 +0800

    Update BackToOriginForTile.cs

--- View/Assets/Hazwan/Scripts/BackToOriginForTile.cs
+++ View/Assets/Hazwan/Scripts/BackToOriginForTile.cs
@@ -4,19 +4,27 @@ using UnityEngine;
 
 public class BackToOriginForTile : BackToOriginal
 {
+    FallingTile tile;
     Rigidbody rb;
+    bool wasFake = false;
     override protected void Start()
     {
         base.Start();
+        tile = GetComponent<FallingTile>();
         rb = GetComponent<Rigidbody>();
     }
     protected override void OnStartLerp()
     {
         base.OnStartLerp();
         rb.isKinematic = true;
+
+        wasFake = tile.isFake;
+        tile.isFake = false;
     }
     protected override void OnEndLerp()
     {
         base.OnEndLerp();
+
+        tile.isFake = wasFake;
     }
 }
