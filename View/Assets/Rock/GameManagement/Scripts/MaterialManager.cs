7222104a9e481ee764317969f3eeaf2ae057c048
Rock <0123956@kdu-online.com>
2020-10-12 19:27:01 +0800

    alex room stuff

--- View/Assets/Rock/GameManagement/Scripts/MaterialManager.cs
+++ View/Assets/Rock/GameManagement/Scripts/MaterialManager.cs
@@ -2,6 +2,8 @@
 
 public class MaterialManager : MonoBehaviour
 {
+    [SerializeField] private Material floorMaterial = null;
+    [SerializeField] private Material ceilingMaterial = null;
     [SerializeField] private Material wallMaterial = null;
     [SerializeField] private Material fadedOutMaterial = null;
     [SerializeField] private Material outLineMaterial = null;
@@ -16,4 +18,6 @@ public class MaterialManager : MonoBehaviour
     public static Material FadedOutMaterial { get { return instance.fadedOutMaterial; } }
     public static Material WallMaterial { get { return instance.wallMaterial; } }
     public static Material OutLineMaterial { get { return instance.outLineMaterial; } }
+    public static Material FloorMaterial { get { return instance.floorMaterial; } }
+    public static Material CeilingMaterial { get { return instance.ceilingMaterial; } }
 }
