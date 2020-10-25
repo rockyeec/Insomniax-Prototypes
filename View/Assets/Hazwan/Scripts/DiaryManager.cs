ce39347adbc9782c4ecdd43f9b94001977997a2d
Wan Hazwan <wahazwan1@gmail.com>
2020-10-26 01:09:11 +0800

    Diary Created - More to add/adjust soon

---
+++ View/Assets/Hazwan/Scripts/DiaryManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DiaryManager : MonoBehaviour
+{
+    protected bool isDiaryOpened = false;
+    protected int currentPage = 0;
+
+
+    protected void ActiveDiary(GameObject diaryContainer)
+    {
+        if (diaryContainer.activeInHierarchy)
+            isDiaryOpened = true;
+        else
+            isDiaryOpened = false;
+
+        print(diaryContainer.activeInHierarchy);
+    }
+
+    
+}
