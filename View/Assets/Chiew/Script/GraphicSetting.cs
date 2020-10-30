7434970e81fe614db60558ffe8d4679753f8d466
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-10-31 04:12:45 +0800

    QualitySetting

--- View/Assets/Chiew/Script/GraphicSetting.cs
+++ View/Assets/Chiew/Script/GraphicSetting.cs
@@ -1,19 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GraphicSetting : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        DefaultQuality();  
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DefaultQuality()
     {
-        
+        GraphicSettingButtons(1);
     }
 
     public void GraphicSettingButtons(int num)
@@ -21,16 +21,19 @@ public class GraphicSetting : MonoBehaviour
         if(num == 1)
         {
             //low
+            QualitySettings.SetQualityLevel(0);
             Debug.Log("Change to Low");
         }
         else if(num == 2)
         {
             //med
+            QualitySettings.SetQualityLevel(3);
             Debug.Log("Change to Med");
         }
         else
         {
             //high
+            QualitySettings.SetQualityLevel(5);
             Debug.Log("Change to High");
         }
     }
