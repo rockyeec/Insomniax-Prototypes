25e31cfd1a8667ae0fb9e63df3a4ed9053adcb0d
Rock <0123956@kdu-online.com>
2020-10-31 12:42:01 +0800

    backup for menu handler rearrangement

---
+++ View/Assets/_Shared/Editor/DesignerToolForMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TempMenuPageHandler))]
+public class DesignerToolForMenu : Editor
+{
+    [MenuItem("Tools/Clear PlayerPrefs")]
+    private static void NewMenuOpeion()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+}
