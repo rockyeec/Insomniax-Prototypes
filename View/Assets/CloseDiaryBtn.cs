fbfd9dc9f612ca6a9349356036a86ae56936d10f
Wan Hazwan <wahazwan1@gmail.com>
2020-11-01 15:39:16 +0800

    Diary is now a super prefab. Still modifying for improvement

---
+++ View/Assets/CloseDiaryBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseDiaryBtn : MonoBehaviour
+{
+    private Button openDiaryBtn = null;
+
+    void Start()
+    {
+        openDiaryBtn = gameObject.GetComponent<Button>();
+
+        openDiaryBtn.onClick.AddListener(CallDiaryFunc);
+    }
+
+    public void CallDiaryFunc()
+    {
+        Diary.Instance.CloseDiary();
+    }
+}
---
+++ View/Assets/OpenDiaryBtn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class OpenDiaryBtn : MonoBehaviour
+{
+    private Button openDiaryBtn = null;
+
+    void Start()
+    {
+        openDiaryBtn = gameObject.GetComponent<Button>();
+
+        openDiaryBtn.onClick.AddListener(CallDiaryFunc);
+    }
+
+    public void CallDiaryFunc()
+    {
+        Diary.Instance.OpenDiary();
+    }
+
+}
