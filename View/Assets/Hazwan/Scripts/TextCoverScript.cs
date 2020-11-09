23a028f76ad4a295d41d5330bcd8b7313c7b6cbf
Wan Hazwan <wahazwan1@gmail.com>
2020-11-09 11:45:01 +0800

    Added outline to objects. More changes later

--- View/Assets/Hazwan/Scripts/TextCoverScript.cs
+++ View/Assets/Hazwan/Scripts/TextCoverScript.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
-using UnityEngine.UI;
 
 public class TextCoverScript : MonoBehaviour
 {
@@ -11,22 +8,66 @@ public class TextCoverScript : MonoBehaviour
     private GameObject diaryContainer;
     private GameObject childContent;
 
-    private int diaryEntryNum = 0;
     bool isTriggered = false;
 
     private ClickableObject clickable = null;
     private InteractableObserver observer = null;
 
-    private void Start()
+    MeshRenderer callTempMeshRend;
+
+    Material[] matArrayOutline;
+    Material[] matArrayNormal;
+
+    void Start()
     {
         CheckSavedData();
         clickable = gameObject.AddComponent<ClickableObject>();
         clickable.Init(this);
         observer = gameObject.AddComponent<InteractableObserver>();
         observer.Init(coverIndex);
-
+        Outline();
         diaryContainer = GameObject.FindGameObjectWithTag("DiaryPackage");
         childContent = GameObject.FindGameObjectWithTag("DiaryContent");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("TriggerCheck") && !isTriggered)
+        {
+            callTempMeshRend.materials = matArrayOutline;
+            clickable.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.CompareTag("TriggerCheck"))
+        {
+            callTempMeshRend.materials = matArrayNormal;
+            clickable.enabled = false;
+        }
+    }
+
+    public void DisableTextCover()
+    {
+        Diary.Instance.currentPage = coverIndex;
+        Diary.Instance.ButtonsVisibility(coverIndex, Diary.diaryContent);
+        Diary.Instance.OpenButton.SetActive(false);
+        Diary.Instance.DiaryEntry.SetActive(true);
+        diaryContainer.transform.GetChild(0).gameObject.SetActive(true);
+        childContent.transform.GetChild(coverIndex).gameObject.SetActive(true);
+        isTriggered = true;
+        observer.Trigger();
+    }
+
+    public void Outline()
+    {
+        callTempMeshRend = GetComponent<MeshRenderer>();
+        matArrayNormal = callTempMeshRend.materials;
+        List<Material> listMaterial = new List<Material>();
+        listMaterial.AddRange(matArrayNormal);
+        listMaterial.Add(MaterialManager.OutLineMaterial);
+        matArrayOutline = listMaterial.ToArray();
     }
 
     void CheckSavedData()
@@ -37,34 +78,4 @@ public class TextCoverScript : MonoBehaviour
             isTriggered = true;
         }
     }
-
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.CompareTag("TriggerCheck") && !isTriggered)
-        {
-            clickable.enabled = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider col)
-    {
-        if (col.gameObject.CompareTag("TriggerCheck"))
-        {
-            clickable.enabled = false;
-        }
-    }
-
-    public void DisableTextCover()
-    {
-        Diary.Instance.currentPage = diaryEntryNum;
-        Diary.Instance.ButtonsVisibility(diaryEntryNum, Diary.diaryContent);
-        Diary.Instance.OpenButton.SetActive(false);
-        Diary.Instance.DiaryEntry.SetActive(true);
-        diaryContainer.transform.GetChild(0).gameObject.SetActive(true);
-        childContent.transform.GetChild(coverIndex).gameObject.SetActive(true);
-        isTriggered = true;
-        observer.Trigger();
-    }
-
-
 }
