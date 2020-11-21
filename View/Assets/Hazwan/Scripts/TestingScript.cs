44c45b13ce8fff4adc1a97968345e72c624138e0
Wan Hazwan <wahazwan1@gmail.com>
2020-11-22 00:28:23 +0800

    tiles updated

---
+++ View/Assets/Hazwan/Scripts/TestingScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    public Dialogue startDialogue;
+    void Start()
+    {
+        DialogueManager.instance.StartDialogue(startDialogue);
+    }
+
+    void Update()
+    {
+        
+    }
+}
