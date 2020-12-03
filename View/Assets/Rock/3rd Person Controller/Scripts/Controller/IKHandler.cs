2292ee249d512e93888e65a794be7c321dafb307
Rock <0123956@kdu-online.com>
2020-12-03 13:39:32 +0800

    solved weird IK during ledge

--- View/Assets/Rock/3rd Person Controller/Scripts/Controller/IKHandler.cs
+++ View/Assets/Rock/3rd Person Controller/Scripts/Controller/IKHandler.cs
@@ -73,6 +73,9 @@ public class IKHandler : MonoBehaviour
 
     private bool CanPerformIK()
     {
-        return Mathf.Abs( animator.GetFloat("vertical")) < 0.03f;
+        return 
+            Mathf.Abs( animator.GetFloat("vertical")) < 0.03f
+            &&
+            !animator.GetBool("ledge");
     }
 }
